950a4f56fdfd796843ebe22e6f39f708a13e6cb2
luanrem <luanrem@gmail.com>
2023-11-20 20:58:11 -0300

    feat: create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+
+import { Home, About, Contact, Projects } from './pages'
+
 import { Navbar } from './components/Navbar'
 
 function App() {
@@ -8,10 +11,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={'Home'} />
-          <Route path='/about' element={'About'} />
-          <Route path='/projects' element={'Projects'} />
-          <Route path='/contact' element={'Contact'} />
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </Router>
     </main>
