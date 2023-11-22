5410c67cbbd2e36e93422ab0c1c830a62ac08782
luanrem <luanrem@gmail.com>
2023-11-21 21:24:53 -0300

    feat: make elements move

--- src/models/Sky.tsx
+++ src/models/Sky.tsx
@@ -1,10 +1,22 @@
+import { useRef } from "react"
+import { useFrame } from "@react-three/fiber"
 import { useGLTF } from "@react-three/drei"
 
 import skyScene from '../assets/3d/sky.glb'
 
-export default function Sky() {
+export default function Sky({ isRotating }: any) {
   const sky = useGLTF(skyScene)
-  return <mesh>
+  const skyRef = useRef()
+
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.25 * delta; // Adjust the rotation speed as needed
+    }
+  });
+
+  return (
+  <mesh ref={skyRef}>
     <primitive object={sky.scene} />
   </mesh>
+)
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,59 @@
-import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
-import { Loader } from '../components/Loader'
-import Island from '../models/Island'
-import Sky from '../models/Sky'
-import Bird from '../models/Bird'
-import Plane from '../models/Plane'
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useState } from "react";
+import { Loader } from "../components/Loader";
+import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
 POPUP
-</div> */}
+</div> */
+}
 
 export default function Home() {
-  const adjustIslandForScreenSize = () => {
-    let screenScale = null 
-    const screenPosition = [0, -6.5, -43]
-    const rotation = [0.1, 4.7, 0]
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
 
-    if(window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9]
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    const screenPosition = [0, -6.5, -43];
+    const rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
     } else {
-      screenScale = [1, 1, 1]
+      screenScale = [1, 1, 1];
     }
 
-    return [screenScale, screenPosition, rotation]
-  }
+    return [screenScale, screenPosition, rotation];
+  };
 
-  const [ islandScale, islandPosition, islandRotation ] = adjustIslandForScreenSize()
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
-      <Canvas 
-        className='w-full h-screen bg-transparent'
+      <Canvas
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
@@ -39,15 +62,23 @@ export default function Home() {
           <hemisphereLight color="#b1e1ff" groundColor="#000" intensity={1} />
 
           <Bird />
-          <Sky />
-          <Island 
+          <Sky isRotating={isRotating} />
+          <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
           />
-          <Plane />
+          <Plane
+            isRotating={isRotating}
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
+          />
         </Suspense>
       </Canvas>
     </section>
-  )
+  );
 }
